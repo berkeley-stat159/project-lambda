146cb07fe647b898cb62e472cf9d4360b4924446
Alon Daks <alon.daks@berkeley.edu>
2015-11-24 00:15:38 -0800

    Prepairing for PR for concatenation preprocessing

--- code/reproduction/preprocess.py
+++ code/reproduction/preprocess.py
@@ -4,6 +4,7 @@ import json
 import nibabel as nib
 import numpy as np
 import sys
+from os.path import exists
 
 DATA_PATHS = json.load(open('../../data/data_path.json'))
 
@@ -18,21 +19,23 @@ def concatenate_runs(alignment='linear'):
         pass
     elif alignment == 'rcds':
         for i, subject in enumerate(DATA_PATHS['subjects']):
-            run_data = []
-            for j, run in enumerate(subject['runs']):
-                task_path = run['rcds']['path']
-                img = nib.load('../../' + task_path)
-                data = img.get_data()
-                if j == 0:
-                    run_data.append(data[..., :-4])
-                elif j >= 1 and j <= 6:
-                    run_data.append(data[..., 4:-4])
-                else:
-                    run_data.append(data[..., 4:])
-            concatenated_run_data = np.concatenate(run_data, axis=3)
-            concatenated_img = nib.Nifti1Image(concatenated_run_data,
-                                               np.eye(4))
-            nib.save(concatenated_img, 'sub{0}_{1}'.format(i + 1, alignment))
+            nii_file_name = 'sub{0}_{1}'.format(i + 1, alignment)
+            if not exists(nii_file_name):
+                run_data = []
+                for j, run in enumerate(subject['runs']):
+                    task_path = run['rcds']['path']
+                    img = nib.load('../../' + task_path)
+                    data = img.get_data()
+                    if j == 0:
+                        run_data.append(data[..., :-4])
+                    elif j >= 1 and j <= 6:
+                        run_data.append(data[..., 4:-4])
+                    else:
+                        run_data.append(data[..., 4:])
+                concatenated_run_data = np.concatenate(run_data, axis=3)
+                concatenated_img = nib.Nifti1Image(concatenated_run_data,
+                                                   np.eye(4))
+                nib.save(concatenated_img, nii_file_name)
     else:
         raise ValueError(CONCATENATE_RUNS_ARG_ERROR_MESSAGE)
 
