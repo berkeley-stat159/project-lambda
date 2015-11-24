219ceaec97f5d9814ac9aac62a451da3f6d3ac24
Alon Daks <alon.daks@berkeley.edu>
2015-11-23 22:51:18 -0800

    Added command line interface to preprocess.py

--- code/reproduction/preprocess.py
+++ code/reproduction/preprocess.py
@@ -3,16 +3,20 @@ from __future__ import division
 import json
 import nibabel as nib
 import numpy as np
+import sys
 
 DATA_PATHS = json.load(open('../../data/data_path.json'))
+
+CONCATENATE_RUNS_ARG_ERROR_MESSAGE = 'Invalid argument: alignment in concatenate_runs'
+PREPROCESS_ARG_ERROR_MESSAGE = 'preprocess.py must be provided with alignment argument'
 
 
 def concatenate_runs(alignment='linear'):
     if alignment == 'linear':
         pass
-    if alignment == 'non_linear':
+    elif alignment == 'non_linear':
         pass
-    if alignment == 'rcds':
+    elif alignment == 'rcds':
         for i, subject in enumerate(DATA_PATHS['subjects']):
             run_data = []
             for j, run in enumerate(subject['runs']):
@@ -29,3 +33,11 @@ def concatenate_runs(alignment='linear'):
             concatenated_img = nib.Nifti1Image(concatenated_run_data,
                                                np.eye(4))
             nib.save(concatenated_img, 'sub{0}_{1}'.format(i + 1, alignment))
+    else:
+        raise ValueError(CONCATENATE_RUNS_ARG_ERROR_MESSAGE)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        raise ValueError(PREPROCESS_ARG_ERROR_MESSAGE)
+    concatenate_runs(sys.argv[1])
