165249505bad32aef9e8f9b84a370b52e0e5c871
Jordeen Chang <jordeenchang@gmail.com>
2015-11-29 11:53:03 -0800

    using textblob library

--- code/utils/sentiment.py
+++ code/utils/sentiment.py
@@ -1,20 +1,45 @@
-from senti_classifier import senti_classifier
 import nltk
 import csv
-sentences = ['The movie was the worst movie', 'It was the worst acting by the actors']
-pos_score, neg_score = senti_classifier.polarity_scores(sentences)
-print pos_score, neg_score
+from textblob import TextBlob
+
 path_to_scene_csv = "../../ds113_study_description/stimulus/task001/annotations/german_audio_description.csv"
 
-def translate_file(filename):
+def get_polarity_dict(filename):
   with open(filename, 'rt') as csvfile:
     reader = csv.DictReader(
         csvfile,
-        fieldnames=['start', 'end', 'german-desc'])
+        fieldnames=['start', 'end', 'german_desc'])
     for row in reader:
-        print row
+        start = float(row['start'])
+        end = float(row['end'])
+        blob = TextBlob(row['german_desc'])
+        translated_blob = blob.translate(to="en")
+        sentiment = get_sentiment(blob)
+        print sentiment
 
-def get_sentiment(phrase):
-    print senti_classifier.polarity_scores(phrase)
+def get_sentiment(blob):
+    sentences = blob.sentences
+    sentence_total = len(sentences)
+    sentiment_total = 0
+    for sentence in sentences:
+        sentiment_total += sentence.sentiment.polarity
+    return float(sentiment_total) / float(sentence_total)
 
-get_sentiment(["hello you are ugly and i hate you", "hello you are beautiful and i love you"])
+
+
+# get_polarity_dict(path_to_scene_csv)
+
+# phrase = "Eine Computeranimation: Auf einen schroffen Berg mit schneebedeckter Flanke fliegt eine Reihe Sterne zu. Sie bilden einen Kranz um den Gipfel: 'Paramount'."
+phrase = "you are ugly"
+
+
+blob = TextBlob(phrase)
+print blob.tags
+print blob.noun_phrases
+for sentence in blob.sentences:
+    print(sentence.sentiment.polarity)
+
+print blob.translate(to="en")
+
+for sentence in blob.sentences:
+    print(sentence.sentiment.polarity)
