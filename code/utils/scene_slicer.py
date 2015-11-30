be5ea63d5acd25a1d48de0351e3fa1efa2050f49
Jordeen Chang <jordeenchang@gmail.com>
2015-11-29 17:44:04 -0800

    removing runs separation

--- code/utils/scene_slicer.py
+++ code/utils/scene_slicer.py
@@ -1,34 +1,28 @@
 import nibabel as nib
 import csv
+import numpy as np
 import json
 
 IS_DAY = 0
 IS_INT = 1
-DAY_IND = 0
-NIGHT_IND = 1
-INT_IND = 2
-EXT_IND = 3
 
 DAY_NIGHT_IND = 0
 INT_EXT_IND = 1
 
-with open('../../data/data_path.json', 'r') as fh:
-    data_paths = json.load(fh)
-path_to_images = []
-for i in range(8):
-    path_to_images.append("../../" + data_paths['bold_dico_7Tad2grpbold7Tad']['sub1']['runs'][0]['path'])
+# with open('../../data/data_path.json', 'r') as fh:
+#     data_paths = json.load(fh)
+# path_to_subject_image = "../../" + data_paths['bold_dico_7Tad2grpbold7Tad']['sub1']['runs'][0]['path']
 
 
 class SceneSlicer:
     def __init__(
             self,
-            path_to_images,
+            path_to_subject_image,
             path_to_scene_csv="../../ds113_study_description/stimulus/task001/annotations/scenes.csv"
     ):
-        self.path_to_images = path_to_images
         self.path_to_scene_csv = path_to_scene_csv
-        self.images = [0] * len(path_to_images)
-        self.scene_slices = [0] * len(path_to_images)
+        self.image = nib.load(path_to_subject_image)
+        self.scene_slices = []
         self.segment_duration = [902, 882, 876, 976, 924, 878, 1086, 673.4]
         self.scene_desc = {}
         self.scene_keys = []
@@ -45,38 +39,19 @@ class SceneSlicer:
         self.scene_keys = list(self.scene_desc.keys())
         self.scene_keys.sort()
 
-    def get_image(self, run_num):
-        if self.images[run_num] == 0:
-            img = nib.load(self.path_to_images[run_num])
-            self.images[run_num] = img
-        return self.images[run_num]
-
-    def get_scene_slices(self, run_num):
-        if not self.images[run_num]:
-            self.get_image(run_num)
+    def get_scene_slices(self):
         if not self.scene_keys:
             self.generate_scene_desc_dict()
-        if not self.scene_slices[run_num]:
-
-            day_slices = []
-            night_slices = []
-            int_slices = []
-            ext_slices = []
-
+        if not self.scene_slices:
             day_night = []
             int_ext = []
-
-            img = self.images[run_num]
-
             key_index = 0
             scene_start = 0
-            for i in range(run_num):
-                scene_start += self.segment_duration[i]
             for i in range(len(self.scene_keys)):
                 if self.scene_keys[i] > scene_start:
                     key_index = i
                     break
-            for i in range(img.shape[3]):
+            for i in range(self.image.shape[3]):
                 if key_index + 1 < len(self.scene_keys) and (
                         i * 2) + scene_start >= self.scene_keys[key_index + 1]:
                     key_index += 1
@@ -87,17 +62,16 @@ class SceneSlicer:
                     IS_INT] else int_ext.append(1)
             print len(day_night)
             print len(int_ext)
-            self.scene_slices[run_num] = (day_night, int_ext)
-        return self.scene_slices[run_num]
+            self.scene_slices = (day_night, int_ext)
+        return self.scene_slices
 
-    def get_day_night(self, run_num, slice):
-        if not self.scene_slices[run_num]:
-            self.get_scene_slices(run_num)
-        scene_slices = self.scene_slices[run_num]
-        is_day_slice = scene_slices[DAY_NIGHT_IND][slice] == 0
-        is_int_slice = scene_slices[INT_EXT_IND][slice] == 0
+    def get_day_night(self, slice):
+        if not self.scene_slices:
+            self.get_scene_slices()
+        is_day_slice = self.scene_slices[DAY_NIGHT_IND][slice] == 0
+        is_int_slice = self.scene_slices[INT_EXT_IND][slice] == 0
         return (is_day_slice, is_int_slice)
 
 
-ss = SceneSlicer(path_to_images)
-print ss.get_scene_slices(0)
+# ss = SceneSlicer(path_to_subject_image)
+# print ss.get_scene_slices()
