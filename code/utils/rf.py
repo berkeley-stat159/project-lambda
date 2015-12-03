c3bee049e67228e290858557a4b05c59a08892f1
Jordeen Chang <jordeenchang@gmail.com>
2015-12-03 04:13:29 -0800

    fixing cross validation adding tests

--- code/utils/rf.py
+++ code/utils/rf.py
@@ -3,17 +3,17 @@ from sklearn.cross_validation import KFold
 import numpy as np
 
 
-def rf_accuracy(X_train, y_train, X_test, y_test):
-    model = RandomForestClassifier(n_estimators=1000,
-                                   max_features=10,
-                                   max_depth=10)
+def rf_accuracy(X_train, y_train, X_test, y_test, est=1000, feat=10, depth=10):
+    model = RandomForestClassifier(n_estimators=est,
+                                   max_features=feat,
+                                   max_depth=depth)
     model.fit(X_train, y_train)
     results = model.score(X_test, y_test)
     return results
 
 
 # Defaults split into 80/20
-def cv_rf_accuracy(X, y, num_folds=5):
+def cv_rf_accuracy(X, y, est=1000, feat=10, depth=10, num_folds=5):
     index_array = np.arange(X.shape[0])
     np.random.shuffle(index_array)
     X = X[index_array]
@@ -21,5 +21,7 @@ def cv_rf_accuracy(X, y, num_folds=5):
     kf = KFold(X.shape[0], n_folds=num_folds)
     avg_acc = 0
     for train, test in kf:
-        avg_acc += rf_accuracy(X[train], y[train], X[test], y[test])
+        avg_acc += rf_accuracy(X[train], y[train], X[test], y[test], est, feat,
+                               depth)
+        print avg_acc
     return avg_acc / float(num_folds)
