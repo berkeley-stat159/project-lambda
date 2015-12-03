d9df3642a91ab7a7e6ce990b245b20d935a1d7e2
Alon Daks <alon.daks@berkeley.edu>
2015-12-03 08:26:55 -0800

    Made code/utils/scene_slicer.py more readable and fixed beginning of movie bug where credits are played to tune machine

--- code/utils/scene_slicer.py
+++ code/utils/scene_slicer.py
@@ -1,7 +1,11 @@
+from __future__ import division
 import nibabel as nib
 import csv
 import numpy as np
 import json
+import math
+
+
 
 IS_DAY = 0
 IS_INT = 1
@@ -15,19 +19,13 @@ INT_EXT_IND = 1
 
 
 class SceneSlicer:
-    def __init__(
-            self,
-            path_to_subject_image,
-            path_to_scene_csv="../../ds113_study_description/stimulus/task001/annotations/scenes.csv"
-    ):
+    def __init__(self, path_to_subject_image, path_to_scene_csv):
         self.path_to_scene_csv = path_to_scene_csv
         self.image = nib.load(path_to_subject_image)
         self.scene_slices = []
-        self.segment_duration = [902, 882, 876, 976, 924, 878, 1086, 673.4]
         self.scene_desc = {}
-        self.scene_keys = []
 
-    def generate_scene_desc_dict(self):
+    def generate_scene_desc_dict_(self):
         with open(self.path_to_scene_csv, 'rt') as csvfile:
             reader = csv.DictReader(
                 csvfile,
@@ -36,31 +34,27 @@ class SceneSlicer:
                 scene_time = int(float(row['seconds']))
                 self.scene_desc[scene_time] = (row['day-night'] == "DAY",
                                                row['int-ext'] == "INT")
-        self.scene_keys = list(self.scene_desc.keys())
-        self.scene_keys.sort()
+
+    def generate_scene_slices_(self):
+        day_night = least_int_great_than_(17/2)*[None]
+        int_ext = least_int_great_than_(17/2)*[None]
+        current_scene_start_time = 17
+        for i in range(9, self.image.shape[-1]):
+            if i*2 - 1 in self.scene_desc:
+                current_scene_start_time = i*2 - 1 
+            elif i*2 in self.scene_desc:
+                current_scene_start_time = i*2
+            day_night.append(self.scene_desc[current_scene_start_time][0])
+            int_ext.append(self.scene_desc[current_scene_start_time][1])
+        day_night = convert_boolean_to_int_array_(day_night)
+        int_ext = convert_boolean_to_int_array_(int_ext)
+        self.scene_slices = (day_night, int_ext)
 
     def get_scene_slices(self):
-        if not self.scene_keys:
-            self.generate_scene_desc_dict()
+        if not self.scene_desc:
+            self.generate_scene_desc_dict_()
         if not self.scene_slices:
-            day_night = []
-            int_ext = []
-            key_index = 0
-            scene_start = 0
-            for i in range(len(self.scene_keys)):
-                if self.scene_keys[i] > scene_start:
-                    key_index = i
-                    break
-            for i in range(self.image.shape[3]):
-                if key_index + 1 < len(self.scene_keys) and (
-                        i * 2) + scene_start >= self.scene_keys[key_index + 1]:
-                    key_index += 1
-                curr_time = self.scene_keys[key_index]
-                day_night.append(0) if self.scene_desc[curr_time][
-                    IS_DAY] else day_night.append(1)
-                int_ext.append(0) if self.scene_desc[curr_time][
-                    IS_INT] else int_ext.append(1)
-            self.scene_slices = (day_night, int_ext)
+            self.generate_scene_slices_()
         return self.scene_slices
 
     def get_day_night(self, slice):
@@ -69,6 +63,18 @@ class SceneSlicer:
         is_day_slice = self.scene_slices[DAY_NIGHT_IND][slice] == 0
         is_int_slice = self.scene_slices[INT_EXT_IND][slice] == 0
         return (is_day_slice, is_int_slice)
+
+def convert_boolean_to_int_array_(array):
+    converted_array = []
+    for elem in array:
+        if elem is None:
+            converted_array.append(elem)
+        else:
+            converted_array.append(elem + 0)
+    return converted_array
+
+def least_int_great_than_(number):
+    return int(math.ceil(number))
 
 # ss = SceneSlicer(path_to_subject_image)
 # print ss.get_scene_slices()
