f9dd34915cf657492032640ee70a21092a234375
Jordeen Chang <jordeenchang@gmail.com>
2015-12-10 13:24:15 -0800

    adjusting files for new make file

--- code/stat159lambda/simulations/correlation_simulation.py
+++ code/stat159lambda/simulations/correlation_simulation.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy import stats
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from stat159lambda.config import REPO_HOME_PATH
 
--- code/stat159lambda/simulations/tests/test_correlation_simulation.py
+++ code/stat159lambda/simulations/tests/test_correlation_simulation.py
@@ -3,6 +3,8 @@ from stat159lambda.simulations import correlation_simulation
 from stat159lambda.config import REPO_HOME_PATH
 
 import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 from numpy.testing import assert_array_equal
 import unittest
 import imp
@@ -14,11 +16,12 @@ except:
 
 
 class SimulateInterRunCorrelationTestCases(unittest.TestCase):
-    @patch.object(matplotlib.pyplot, 'savefig')
+    @patch.object(plt, 'savefig')
     def test_simulate_inter_run_correlation(self, mock_savefig):
         correlation_simulation.simulate_inter_run_correlation()
         assert_array_equal(
             mock_savefig.call_args[0][0],
             '{0}/figures/sim_inter_run.png'.format(REPO_HOME_PATH))
-        main_module = imp.load_source('__main__', '../correlation_simulation.py'.format(REPO_HOME_PATH))
+        correlation_simulation_path = '{0}/code/stat159lambda/simulations/correlation_simulation.py'.format(REPO_HOME_PATH)
+        main_module = imp.load_source('__main__', correlation_simulation_path)
         self.assertEqual(main_module.__name__, '__main__')
