77578bfbf9057d00424aa7cee674e1d4f1a76069
Jordeen Chang <jordeenchang@gmail.com>
2015-12-10 12:09:16 -0800

    reviewing comments

--- code/stat159lambda/simulations/tests/test_correlation_simulation.py
+++ code/stat159lambda/simulations/tests/test_correlation_simulation.py
@@ -15,12 +15,10 @@ except:
 
 class SimulateInterRunCorrelationTestCases(unittest.TestCase):
     @patch.object(matplotlib.pyplot, 'savefig')
-    def test_simulate_inter_run_correlation(self, mock_sim):
+    def test_simulate_inter_run_correlation(self, mock_savefig):
         correlation_simulation.simulate_inter_run_correlation()
         assert_array_equal(
-            mock_sim.call_args[0][0],
+            mock_savefig.call_args[0][0],
             '{0}/figures/sim_inter_run.png'.format(REPO_HOME_PATH))
-         # ('{0}/code/stat159/simulations/correlation_simulation.py'.format(REPO_HOME_PATH))
-
         main_module = imp.load_source('__main__', '../correlation_simulation.py'.format(REPO_HOME_PATH))
         self.assertEqual(main_module.__name__, '__main__')
