ac9060dd723a5f4bc3a8ad8967e5597c2c708452
Ying <ying.t.luo@gmail.com>
2015-12-12 15:48:28 -0800

    Updated documentation for data_path.py

--- code/stat159lambda/utils/data_path.py
+++ code/stat159lambda/utils/data_path.py
@@ -58,11 +58,37 @@ def get_smoothed_path(subj_num, fwhm_mm):
 
 
 def get_smoothed_2d_path(subj_num, fwhm_mm):
+    """
+    Derives the absolute path to the smoothed 2-D data for a particular subject
+    and particular full width half maximum smoothed version
+
+    Parameters
+    ----------
+    subj_num : int
+    fwhm_mm : int
+
+    Returns
+    -------
+    path: string
+    """
     return '{0}/data/processed/sub{1}_rcds_smoothed_{2}_mm_2d.npy'.format(
         REPO_HOME_PATH, subj_num, fwhm_mm)
 
 
 def get_correlation_path(subj_1_num, subj_2_num):
+    """
+    Derives the absolute path to the calculated correlations between two
+    subjects
+
+    Parameters
+    ----------
+    subj_1_num : int
+    subj_2_num : int
+
+    Returns
+    -------
+    path: string
+    """
     return '{0}/data/processed/sub{1}_sub{2}_correlation.npy'.format(
         REPO_HOME_PATH, subj_1_num, subj_2_num)
 
