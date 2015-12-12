fbb388543f519c37e4f8635b2043522b15d14c0d
Ying <ying.t.luo@gmail.com>
2015-12-12 14:28:04 -0800

    Added documentation to data_path.py

--- code/stat159lambda/utils/data_path.py
+++ code/stat159lambda/utils/data_path.py
@@ -5,6 +5,18 @@ DATA_PATHS = json.load(open('{0}/data/data_path.json'.format(REPO_HOME_PATH)))
 
 
 def get_raw_path(subj_num, run_num):
+    """
+    Derives the absolute path to data for a particular subject and run
+
+    Parameters
+    ----------
+    subj_num : int
+    run_num : int
+
+    Returns
+    -------
+    path : string
+    """
     if subj_num < 10:
         subj_num = '0' + str(subj_num)
     return '{0}/data/raw/sub0{1}/task001_run00{2}/bold_dico_dico_rcds_nl.nii'.format(
@@ -12,15 +24,51 @@ def get_raw_path(subj_num, run_num):
 
 
 def get_concatenated_path(subj_num):
+    """
+    Derives the absolute path to data for a particular subject only
+
+    Parameters
+    ----------
+    subj_num : int
+
+    Returns
+    -------
+    path : string
+    """
     return '{0}/data/processed/sub{1}_rcds.npy'.format(REPO_HOME_PATH,
                                                         subj_num)
 
 
 def get_smoothed_path(subj_num, fwhm_mm):
+    """
+    Derives the absolute path to the smoothed data for a particular subject and
+    particular smoothing
+
+    Parameters
+    ----------
+    subj_num : int
+    fwhm_mm : int
+
+    Returns
+    -------
+    path: string
+    """
     return '{0}/data/processed/sub{1}_rcds_smoothed_{2}_mm.npy'.format(
         REPO_HOME_PATH, subj_num, fwhm_mm)
 
 
 def get_2d_path(subj_num):
+    """
+    Derives the absolute path to the 2-D data for a particular subject,
+    originally contained in a 4-D array
+
+    Parameters
+    ----------
+    subj_num : int
+
+    Returns
+    -------
+    path : string
+    """
     return '{0}/data/processed/sub{1}_rcds_2d.npy'.format(REPO_HOME_PATH,
                                                            subj_num)
