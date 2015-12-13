5f09aa2940b479a4b24a47128278505405ce928f
Jordeen Chang <jordeenchang@gmail.com>
2015-12-12 23:58:49 -0800

    comments and adjustments

--- code/stat159lambda/linear_modeling/linear_modeling.py
+++ code/stat159lambda/linear_modeling/linear_modeling.py
@@ -6,26 +6,35 @@ import numpy.linalg as npl
 import nibabel as nib
 import stat159lambda.utils.scene_slicer as ssm
 import stat159lambda.utils.data_path as dp
-from stat159lambda.config import REPO_HOME_PATH
-from scipy.stats import t
+from stat159lambda.config import REPO_HOME_PATH, NUM_OFFSET_VOLUMES
+
 
 class VoxelExtractor:
 
-    def __init__(self, subject, interest_col_str):
+    def __init__(self, subject, interest_col_str, data=None):
+        """
+        VoxelExtractor generates the t-statistic for each voxel based on a
+        given interest column. For example, to see the voxel that can best
+        predict interior or exterior, you would use the following code.
+
+        ve = VoxelExtractor(1, 'int-ext')
+        a = ve.t_stat()[0]
+        ve.plot_single_voxel(0)
+
+        interest_col_str takes in either 'day-night' or 'int-ext'.
+        data can also be overrided if needed, else the smoothed 2d version
+        will be used.
+        """
         self.subject = subject
         self.interest_col_str = interest_col_str
-        data_path = dp.get_smoothed_path_2d(self.subject, 4)
-        data_path = "/Users/Jordeen/stat159/jodreen-work/project-lambda/data/raw/sub001/task001_run001/bold_dico_dico_rcds_nl.nii"
-        data = nib.load(data_path).get_data()
-        data = data[:, :, :, 400:]
+        if not data:
+            data_path = dp.get_smoothed_path_2d(self.subject, 4)
+            data = nib.load(data_path).get_data()
+            data = data[:, :, :, NUM_OFFSET_VOLUMES:]
         # Data is shaped as number of voxels by time
         self.data = np.reshape(data, (-1, data.shape[-1]))
         self.design = None
         self.B = None
-
-    def lol(self):
-        # t_vals = np.zeroes(self.data[0])
-        print(t.stats(self.data[0, :]))
 
     def get_design_matrix(self):
         """
@@ -47,12 +56,13 @@ class VoxelExtractor:
             n_trs = self.data.shape[-1]
             design = np.ones((n_trs, 3))
             design[:, 1] = np.linspace(-1, 1, n_trs)
-            design[:, 2] = interest_col[400:451]
+            design[:, 2] = interest_col[NUM_OFFSET_VOLUMES:]
             self.design = design
         return self.design
 
     def plot_design_matrix(self):
         """
+        Saves the design matrix as an image into the figures folder.
         Returns
         -------
         None
@@ -79,29 +89,6 @@ class VoxelExtractor:
             self.B = npl.pinv(self.design).dot(Y.T)
         return self.B
 
-    def get_betas_4d(B, data):
-        """
-        Returns
-        -------
-        4D array, beta for each voxel; need this format to plot
-        """
-        return np.reshape(B.T, data.shape[:-1] + (-1, ))
-
-    def plot_betas(self, b_vols, col):
-        """
-        Parameters
-        ----------
-        b_vols: 2D array
-        col: integer between 0 and p
-        Returns
-        -------
-        None
-        """
-        if col >= b_vols.shape[-1]:
-            raise RuntimeError("Error: select a column between 0 and p")
-        c = b_vols.shape[2] // 2
-        plt.imshow(b_vols[:, :, c, col], cmap='gray', interpolation='nearest')
-
     def t_stat(self):
         """ betas, t statistic and significance test given data,
         design matix, contrast
@@ -121,7 +108,6 @@ class VoxelExtractor:
         RSS = (errors**2).sum(axis=0)
         df = X.shape[0] - npl.matrix_rank(X)
         MRSS = RSS / df
-        # calculate bottom half of t statistic
         SE = np.sqrt(MRSS * c.T.dot(npl.pinv(X.T.dot(X)).dot(c)))
         SE[SE == 0] = np.amin(SE[SE != 0])
         t = c.T.dot(beta) / SE
@@ -131,20 +117,14 @@ class VoxelExtractor:
 
     def plot_single_voxel(self, voxel_index):
         """
+        Plots a single voxel timecourse and the figure is saved under the
+        figures folder.
         Returns
         -------
         None
         """
         voxel_img = plt.gcf()
         plt.plot(self.data[voxel_index, :])
-        print(self.data[voxel_index, :])
         voxel_img_path = '{0}/figures/voxel_{1}.png'.format(REPO_HOME_PATH, voxel_index)
         voxel_img.savefig(voxel_img_path, dpi=100)
         plt.clf()
-
-
-ve = VoxelExtractor(1, 'int-ext')
-# ve.lol()
-for i in range(100):
-    a = ve.t_stat()[i]
-    ve.plot_single_voxel(a)
