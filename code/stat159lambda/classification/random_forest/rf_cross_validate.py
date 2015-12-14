6469beb2a5f191cfcee8cd52ed82ce1be3a6e836
Alon Daks <alon.daks@berkeley.edu>
2015-12-14 07:50:26 -0800

    finished refactored rf_cross_validate script

--- code/stat159lambda/classification/random_forest/rf_cross_validate.py
+++ code/stat159lambda/classification/random_forest/rf_cross_validate.py
@@ -9,30 +9,42 @@ from stat159lambda.config import REPO_HOME_PATH, NUM_VOXELS
 from stat159lambda.linear_modeling import linear_modeling as lm
 from stat159lambda.utils import data_path as dp
 
+NUM_FEATURES = [500, 1000, 2500, 5000, 10000, 20000, 30000, 40000, 42000,
+                44000, 46000, 48000, 50000]
 
-voxels_sorted_by_t_statistic = lm.VoxelExtractor(1, 'int-ext').t_stat()
-num_features_values = [200, 400, 600, 1000, 1500, 2000, 3000, 5000, 10000]
-design_matrix = dm.DesignMatrix(dp.get_smoothed_2d_path(1, 4))
-train_volume_indices = pv.get_train_indices()
-cv_values = []
-for num_features in num_features_values:
-	voxel_feature_indices = voxels_sorted_by_t_statistic[:num_features]
 
-	X_train = design_matrix.get_design_matrix(train_volume_indices, voxel_feature_indices)
-	y_train = np.array(design_matrix.get_labels(train_volume_indices))
+def main():
+    subj_num, fwhm_mm = 1, 4
+    voxels_sorted_by_t_statistic = lm.VoxelExtractor(subj_num,
+                                                     'int-ext').t_stat()
+    design_matrix = dm.DesignMatrix(dp.get_smoothed_2d_path(subj_num, fwhm_mm))
+    train_volume_indices = pv.get_train_indices()
+    cv_values = []
+    for num_features in NUM_FEATURES:
+        voxel_feature_indices = voxels_sorted_by_t_statistic[:num_features]
 
-	cv_accuracies = []
-	for train, test in KFold(len(X_train), 5):
-		X_cv_train = X_train[train, :]
-		y_cv_train = y_train[train]
-		X_cv_test = X_train[test, :]
-		y_cv_test = y_train[test]
-		model = rf.Classifier(X_cv_train, y_cv_train)
-		model.train()
-		y_predicted = model.predict(X_cv_test)
-		cv_accuracies.append(accuracy_score(y_predicted, y_cv_test))
+        X_train = design_matrix.get_design_matrix(train_volume_indices,
+                                                  voxel_feature_indices)
+        y_train = np.array(design_matrix.get_labels(train_volume_indices))
 
-	cv_values.append(np.mean(cv_accuracies))
+        cv_accuracies = []
+        for train, test in KFold(len(X_train), 5):
+            X_cv_train = X_train[train, :]
+            y_cv_train = y_train[train]
+            X_cv_test = X_train[test, :]
+            y_cv_test = y_train[test]
+            model = rf.Classifier(X_cv_train, y_cv_train)
+            model.train()
+            y_predicted = model.predict(X_cv_test)
+            cv_accuracies.append(accuracy_score(y_predicted, y_cv_test))
 
-np.save('cv_values', cv_values)
+        print(np.mean(cv_accuracies))
+        cv_values.append(np.mean(cv_accuracies))
+    output_path = '{0}/figures/rf_cross_validated_accuracies.txt'.format(
+        REPO_HOME_PATH)
+    np.savetxt(output_path, cv_values)
+    print('Saved {0}'.format(output_path))
 
+
+if __name__ == '__main__':
+    main()
